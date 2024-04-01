ab96588c26955f0de8bc99a7fe6e9ae2d4706640
James Tan <james@predictivehire.com>
2024-04-01 17:46:46 +1100

    feat: sort articles

---
+++ articles/index.md
@@ -0,0 +1,10 @@
+---
+outline: false
+prev: false
+next: false
+lastUpdated: false
+editLink: false
+---
+# Articles
+
+Click left `sidebar` to select on article to read.
--- scripts/genSidebar.js
+++ scripts/genSidebar.js
@@ -12,11 +12,25 @@ MDFiles.forEach((filePath) => {
   const text = ((fileContent.match(/^#\s*(.*)/m) || [])[1] || '').trim();
 
   if (!text) {
-    throw new Error('No #title found in file: ' + filePath)
+    throw new Error('No #title found in file: ' + filePath);
   }
-  
-  const obj = { text, link: `/articles/${filename}.md` };
-  items.push(obj);
+
+  if (filename !== 'index') {
+    const fileStat = fs.statSync(filePath);
+    const updatedAt = fileStat.mtime;
+
+    const obj = {
+      text,
+      link: `/articles/${filename}.md`,
+      updatedAt: updatedAt.toISOString(),
+    };
+    items.push(obj);
+  }
+});
+
+// Sort the items array based on the updatedAt field in descending order
+items.sort((a, b) => {
+  return new Date(b.updatedAt) - new Date(a.updatedAt);
 });
 
 fs.writeFileSync(pwd + '/.vitepress/sidebar.json', JSON.stringify(items));
